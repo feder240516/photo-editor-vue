af73a752fd13c087e09ef3c959d3db2bd61a7689
Federico Reina <federicoreinavinchira@gmail.com>
2024-05-12 19:42:04 -0500

    chore(web): removed unneeded files, added tailwind, setup of tsconfig

---
+++ src/components/canvas/CanvasComponent.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue';
+const height = ref(640);
+const width = ref(640);
+</script>
+<template>
+  <div
+    :style="{
+      width: `${width}px`,
+      height: `${height}px`,
+      backgroundColor: 'red',
+    }"
+  ></div>
+</template>
+<style></style>
---
+++ src/views/CanvasView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import CanvasComponent from '../components/canvas/CanvasComponent.vue';
+</script>
+
+<template>
+  <main class="min-h-screen w-full flex items-center justify-center">
+    <CanvasComponent />
+  </main>
+</template>
